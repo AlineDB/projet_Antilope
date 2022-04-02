d910381b52e2d7b57c81b30e52ff61c5c150f683
AlineDB <71900695+AlineDB@users.noreply.github.com>
2022-04-02 16:38:41 +0200

    gitignore ok

--- antilope/wp-content/themes/antilope/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-?>
-<footer class="footer">
-    <h2 class="footer__title">Pied de page</h2>
-</footer>
---
+++ wp-content/themes/antilope/footer.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+<footer class="footer">
+    <h2 class="footer__title">Pied de page</h2>
+    <div class="footer__container">
+        <ul class="footer__list">
+            <li class="footer__li">
+                <a href="https://www.issep.be/"><img src="../../uploads/2022/04/logo_ISSEP.jpg" alt="Logo de la ISSEP"></a>
+            </li>
+            <li class="footer__li">
+                <a href="https://www.ingehepl.be/master-en-sciences-de-lingenieur-industriel-orientation-electronique-systemes-embarques/"><img src="../../uploads/2022/04/logo_electro.jpg" alt="Logo du service électronique"></a>
+            </li>
+            <li class="footer__li">
+                <a href="https://www.hepl.be/fr/formations"><img src="../../uploads/2022/04/logo_HEPL.jpg" alt="Logo de la HEPL"></a>
+            </li>
+        </ul>
+    </div>
+</footer>
